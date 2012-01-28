6f48f41b2dae6792bfe64c38f2948134de70aced
Jim Griswold <jim.griswold@gmail.com>
2012-01-28 11:35:49 -0500

    only complete from known_hosts if exists

--- ssh/completion.sh
+++ ssh/completion.sh
@@ -1,2 +1,3 @@
 # allows completion for any host in your known_hosts file
-complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g -e s/"\["//g -e s/"\]".*//g | uniq`;)" ssh
+KNOWN_HOSTS=$HOME/.ssh/known_hosts
+[[ -f $KNOWN_HOSTS ]] && complete -W "$(echo `cat $KNOWN_HOSTS | cut -f 1 -d ' ' | sed -e s/,.*//g -e s/"\["//g -e s/"\]".*//g | uniq`;)" ssh
